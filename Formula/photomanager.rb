9e66b063786819350f1a83d283bc6a456d2d1a6a
Maksim Vakarchuk <11082252+maks112v@users.noreply.github.com>
2024-03-09 17:37:56 -0500

    brew tap

--- Formula/photomanager.rb
+++ Formula/photomanager.rb
@@ -1,8 +1,9 @@
-class PhotoManager < Formula
+class Photomanager < Formula
   desc 'Photo manager for organizing photos into folders by date'
   homepage 'https://github.com/maks112v/photo-manager'
-  url 'https://github.com/maks112v/photo-manager'
-  sha256 'SHA256 hash of the tarball'
+  url 'https://github.com/maks112v/photo-manager/archive/refs/tags/2024.1.tar.gz'
+  sha256 '1f71638277d60cc87c9d508307e041b4603e1115a2d308a5af9c9c13559141e8'
+  version '2024.1'
 
   depends_on 'go' => :build
 
